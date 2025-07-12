0838505a80cc1c6cd1a07cf2700d7d739e14559d
⨀⃝⃟⃞༺𝔜𝔞𝔰𝔲༻𝔣𝔣❀ <jusuecastrocg@gmail.com>
2025-07-12 05:19:44 -0600

    Marin-Bot info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,23 +1,18 @@
-import fetch from 'node-fetch'; 
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-
     try {
         await m.react('🧐');
     } catch (e) {
         console.error("Error al reaccionar:", e);
     }
 
-
     const senderName = await conn.getName(m.sender);
-
-
-    const ownerName = `${etiqueta}`;
-    const ownerPhoneNumber = "50577025053"; 
-    const ownerEmail = "x"; 
+    const ownerName = `${global.etiqueta || 'Saki_Ayase-Bot Owner'}`;
+    const ownerPhoneNumber = "50577025053";
+    const ownerEmail = "x";
     const ownerGithubUrl = "https://github.com/Yasu-jc/Saki_Ayase-Bot";
     const ownerRegion = "Nicaragua";
-
 
     const ownerVcard = `
 BEGIN:VCARD
@@ -32,36 +27,35 @@ item4.ADR:;; ${ownerRegion};;;;
 item4.X-ABLabel:Región
 END:VCARD`;
 
-
     const contactsList = [{
         displayName: ownerName,
         vcard: ownerVcard,
     }];
 
-
-    await conn.sendMessage(m.chat, {
+    
+    const contactMsg = await conn.sendMessage(m.chat, {
         contacts: {
             displayName: `${contactsList.length} Contacto`,
             contacts: contactsList
         },
         contextInfo: {
             externalAdReply: {
-                showAdAttribution: true,
                 title: `ΉӨᒪΛ, ƧӨY:\n${global.wm || 'Saki_Ayase-Bot'}`,
-                body: global.dev || 'Desarrollador', 
-                thumbnailUrl: 'https://i.postimg.cc/5ttCGWzS/6566a3679298a46030e51d633757bcd4.jpg', 
+                body: global.dev || 'Desarrollador',
+                thumbnailUrl: 'https://i.postimg.cc/5ttCGWzS/6566a3679298a46030e51d633757bcd4.jpg',
                 sourceUrl: `https://wa.me/${ownerPhoneNumber}?text=Vengo+Del+Comando+.owner`,
                 mediaType: 1,
                 renderLargerThumbnail: true
             }
         }
-    }, {
-        quoted: m
-    });
+    }, { quoted: m });
 
-
+    
     const welcomeMessage = `👋 *Hola \`${senderName}\` este es*\n*el contacto de mi creador*`;
-    await conn.sendMessage(m.chat, { text: welcomeMessage });
+    await conn.sendMessage(m.chat, {
+        text: welcomeMessage,
+        quoted: contactMsg 
+    });
 };
 
 handler.help = ['owner', 'creator'];
