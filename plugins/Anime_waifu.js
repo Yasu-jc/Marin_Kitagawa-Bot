d83f69e54ad75a19f9570b73a4ee3d59cc4caf25
⨀⃝⃟⃞༺𝔜𝔞𝔰𝔲༻𝔣𝔣❀ <jusuecastrocg@gmail.com>
2026-05-11 21:38:54 -0600

    Refactor Anime_waifu.js to new command structure

--- plugins/Anime_waifu.js
+++ plugins/Anime_waifu.js
@@ -1,17 +1,5 @@
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, usedPrefix, command }) => {
-try {
-await m.react(emojis)
-let res = await fetch('https://api.waifu.pics/sfw/waifu')
-if (!res.ok) return
-let json = await res.json()
-if (!json.url) return 
-await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', '✦ *W A I F U* ✦', fkontak, null, )
-} catch {
-}}
-handler.help = ['waifu']
-handler.tags = ['anime']
-handler.command = ['waifu2']
-handler.register = false 
-export default handler
+export default {
+  command: ['cmd1', 'cmd2'],
+  category: 'x',
+  run: async () => {}
+}
